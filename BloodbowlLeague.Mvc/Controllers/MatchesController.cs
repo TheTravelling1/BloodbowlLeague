892c1ffa090f8efb1fbf583a865727fa1c4b3967
Peter Stephenson <Peter Stephenson>
2016-12-19 16:31:01 +0000

    wip - Convert Web to Mvc

---
+++ BloodbowlLeague.Mvc/Controllers/MatchesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace BloodbowlLeague.Mvc.Controllers
+{
+    public class MatchesController : Controller
+    {
+        // GET: Match
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult r1g4()
+        {
+            return View();
+        }
+
+        public ActionResult r1g1()
+        {
+            return View();
+        }
+    }
+}
---
+++ BloodbowlLeague.Mvc/Controllers/TeamsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace BloodbowlLeague.Mvc.Controllers
+{
+    public class TeamsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
